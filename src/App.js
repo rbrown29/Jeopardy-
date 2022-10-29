f148ba2304fc8378b3b5aa9c7457888e7f332986
Richard Brown <richardbrown@Richard.local>
2022-10-28 20:53:45 -0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor (props) {
+    super (props)
+    this.state = {
+      items: [],
+      isLoaded: false,
+      isHidden: false,
+      Score: 0
+
+    }
+  this.toggleQuestion = this.toggleQuestion.bind(this)
+  this.increaseScore = this.increaseScore.bind(this)
+  this.decreaseScore = this.decreaseScore.bind(this)
+  this.resetScore = this.resetScore.bind(this)
+  this.componentDidMount = this.componentDidMount.bind(this)
+  this.newQuestion = this.newQuestion.bind(this)
+  }
+
+componentDidMount() {
+  fetch('http://jservice.io/api/random')
+  .then(responce => responce.json())
+  .then(json => {
+  this.setState({
+      items: json,
+      isLoaded: true, 
+    })
+  }).catch((error) => {
+      console.log(error);
+  });
+} 
+toggleQuestion () {
+    this.setState({
+      isHidden: !this.state.isHidden
+    })
+  }
+increaseScore () {
+  this.setState({
+    Score: this.state.Score += this.state.items[0].value
+  })
 }
+decreaseScore () {
+  this.setState({
+    Score: this.state.Score -= this.state.items[0].value
+  })
+}
+resetScore () {
+  this.setState({
+    Score: this.state.Score = 0
+  })
+}
+newQuestion () {
+  this.componentDidMount()
+  this.toggleQuestion()
+}
+
+render () {
+  const {isLoaded, items} = this.state;
+  if(!isLoaded) {
+    return <div>No Data!</div>
+  } else {
+  return (
+    console.log(items),
+    <div className="App">
+       <h1>Welcome to Jeopardy</h1>
+       <br />
+       <h2>Score: {this.state.Score}</h2>
+       <button onClick={this.decreaseScore}>Decrease</button>
+       <button onClick={this.increaseScore}>Increase</button>
+       <button onClick={this.resetScore}>Reset</button><br /><br />
+       <h2>Lets Play!</h2><br />
+
+       <button className="button" onClick={this.newQuestion}>New Question</button>
+       <h2>Category: {items[0].category.title}</h2><br />
+      <h2>Points: {items[0].value}</h2>
+
+       {this.state.isHidden && (
+        <div>
+           <h3>Answer: {items[0].answer}</h3> <br /><br />
+        </div>
+        )}
+       <h3>Question: {items[0].question}</h3><br /><br /> 
+        <button className="button" onClick={this.toggleQuestion}>Reveal Answer</button> 
+    </div>
+  )
+}
+}
+}
+
 
 export default App;
